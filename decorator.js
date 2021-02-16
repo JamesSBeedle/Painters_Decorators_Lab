eb56f29391e184ac5d62dd8d2ccbfd85674d949d
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-16 20:45:10 +0000

    commit for homework

--- decorator.js
+++ decorator.js
@@ -1,10 +1,33 @@
+const Room = require("./room")
+
 const Decorator = function () {
     this.stock = []
 }
 
-Decorator.prototype.addPaintToStock = function (paint) {
-    this.stock.push(paint)
-    return this.stock.length;
+Decorator.prototype.paintInStock = function () {
+    return this.stock.length
 }
+
+Decorator.prototype.addPaintToStock = function (paint) {
+    this.stock.push(paint);
+    return this.paintInStock()
+    
+}
+
+Decorator.prototype.totalLitresInStock = function () {
+    let totalLitres = this.paintInStock() * 15
+    return totalLitres
+}
+
+Decorator.prototype.enoughPaint = function (room) {
+    if (this.totalLitresInStock() >= room.area) {
+        return true, room.painted = true
+    }
+    else{
+        return false
+    }
+}
+
+
 
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,5 +1,7 @@
 const assert = require('assert')
 const Decorator = require('../decorator.js')
+const Paint = require('../paint.js')
+const Room = require('../room.js')
 
 describe('Decorator', function () {
 
@@ -7,16 +9,43 @@ describe('Decorator', function () {
 
     beforeEach(function () {
         decorator = new Decorator()
+        paint1 = new Paint(15)
+        room = new Room(10)
     })
 
     it('should start with no stock', function () {
-        const actual = decorator.stock;
-        assert.deepStrictEqual(actual, [])
+        const actual = decorator.paintInStock();
+        assert.deepStrictEqual(actual, 0)
     })
 
     it('should be able to add a can of paint to stock', function () {
-        decorator.addPaintToStock(paint1)
-        const actual = decorator.stock;
+        const actual = decorator.addPaintToStock(paint1)        
         assert.strictEqual(actual, 1)
     })
+
+    it('should be able to calculate the total number of litres in stock', function () {
+        decorator.addPaintToStock(paint1);
+        const actual = decorator.totalLitresInStock();
+        assert.strictEqual(actual, 15)
+    })
+
+    it('should have enough paint to paint room', function() {
+        decorator.addPaintToStock(paint1)
+        const actual = decorator.enoughPaint(room);
+        assert.strictEqual(actual, true)
+    })
+
+
+    it('should be able to paint room', function () {
+        decorator.addPaintToStock(paint1);
+        decorator.enoughPaint(room);
+        const actual = room.painted
+        assert.strictEqual(actual, true)
+    })
+   
+
+   
+
+
+
 })
